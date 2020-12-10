5a6fbe1bd5593859dc891191cd97dffa96a3dfba
Seiji Naganuma <snaganumas@gmail.com>
2020-12-09 22:05:03 -1000

    remove unneeded manifest plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,7 +10,6 @@ module.exports = {
     "gatsby-plugin-image",
     "gatsby-plugin-react-helmet",
     "gatsby-plugin-sitemap",
-    "gatsby-plugin-manifest",
     "gatsby-plugin-sharp",
     "gatsby-transformer-sharp",
     {
