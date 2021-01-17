27c7110f87df9404c76c41950d10bc3ff8e135bb
Seiji Naganuma <snaganumas@gmail.com>
2021-01-16 21:52:54 -1000

    use gatsby-image for the location photos

--- src/pages/location.js
+++ src/pages/location.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { graphql } from 'gatsby'
 
 import Layout from 'components/layout';
 import LocationHeader from 'components/location/header';
@@ -6,13 +7,33 @@ import DesktopNav from 'components/shared/desktopNav';
 import MobileNav from 'components/shared/mobileNav';
 import LocationMain from 'components/location/main';
 
-const LocationPage = () => (
+const LocationPage = ({ data }) => (
   <Layout>
     <LocationHeader />
     <DesktopNav />
     <MobileNav />
-    <LocationMain />
+    <LocationMain data={data}/>
   </Layout>
 );
+
+export const query = graphql`
+  query {
+    location1: file(relativePath: { eq: "location1.jpg" }) {
+      childImageSharp {
+        gatsbyImageData(layout: FLUID, maxWidth: 580, placeholder: BLURRED)
+      }
+    }
+    location2: file(relativePath: { eq: "location2.jpg" }) {
+      childImageSharp {
+        gatsbyImageData(layout: FLUID, maxWidth: 580, placeholder: BLURRED)
+      }
+    }
+    location3: file(relativePath: { eq: "location3.jpg" }) {
+      childImageSharp {
+        gatsbyImageData(layout: FLUID, maxWidth: 580, placeholder: BLURRED)
+      }
+    }
+  }
+`;
 
 export default LocationPage;
