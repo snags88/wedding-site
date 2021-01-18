73ab064f8d32e35a272837d2b522f2e1996fda9f
Seiji Naganuma <snaganumas@gmail.com>
2021-01-17 22:59:50 -1000

    add address and phone fields (#21)
    * add address and phone fields
    
    * make sure left and right margins are the same

--- src/components/shared/rsvp/index.js
+++ src/components/shared/rsvp/index.js
@@ -3,6 +3,7 @@ import React from 'react';
 import {
   RsvpSection,
   HiddenTextInput,
+  FormContentGrid,
 } from './style';
 
 const Rsvp = ({ isRsvpPage }) => {
@@ -33,45 +34,47 @@ const Rsvp = ({ isRsvpPage }) => {
                 >
                   <input type='hidden' name='form-name' value={formName} />
                   <HiddenTextInput type='text' name='syrup' />
-                  <div className='d-md-flex justify-content-between'>
-                    <div className='rsvp-form__left'>
-                      <div className='rsvp-form-field'>
-                        <input type='text' name='fullname' placeholder='Full Name' required />
-                      </div>
-                      <div className='rsvp-form-field'>
-                        <input type='email' name='email' placeholder='Your E-mail' required />
-                        <p className='rsvp-form-field__error'>E-mail is incorrect.</p>
-                      </div>
-                      <div className='rsvp-form-field'>
-                        <input type='number' name='guests' placeholder='Number of guests' min='0' max='10' required />
-                      </div>
-                      <div className='rsvp-form-field'>
-                        <select name='attending' required>
-                          <option value='' disabled selected>Are you attending?</option>
-                          <option value='all'>Yes - Both!</option>
-                          <option value='ceremony'>Yes - Ceremony only</option>
-                          <option value='reception'>Yes - Reception only</option>
-                          <option value='no'>No</option>
-                        </select>
-                      </div>
+                  <FormContentGrid>
+                    <div className='rsvp-form-field'>
+                      <input type='text' name='fullname' placeholder='Full Name' required />
                     </div>
-                    <div className='rsvp-form__right'>
-                      <div className='rsvp-form-field h-50'>
-                        <textarea
-                          rows='4'
-                          placeholder='Any accommodations such as wheelchair access or dietary restrictions?'
-                          name='comments'
-                        />
-                      </div>
-                      <div className='rsvp-form-field h-50'>
-                        <textarea
-                          rows='4'
-                          placeholder='Your Message to the bride and groom'
-                          name='message'
-                        />
-                      </div>
+                    <div className='rsvp-form-field'>
+                      <input type='email' name='email' placeholder='Your E-mail' required />
+                      <p className='rsvp-form-field__error'>E-mail is incorrect.</p>
                     </div>
-                  </div>
+                    <div className='rsvp-form-field'>
+                      <input type='text' name='address' placeholder='Home address to send invites' required />
+                    </div>
+                    <div className='rsvp-form-field'>
+                      <input type='tel' name='phone' placeholder='Phone Number' required />
+                    </div>
+                    <div className='rsvp-form-field'>
+                      <select name='attending' required>
+                        <option value='' disabled selected>Are you attending?</option>
+                        <option value='all'>Yes - Both!</option>
+                        <option value='ceremony'>Yes - Ceremony only</option>
+                        <option value='reception'>Yes - Reception only</option>
+                        <option value='no'>No</option>
+                      </select>
+                    </div>
+                    <div className='rsvp-form-field'>
+                      <input type='number' name='guest count' placeholder='Number of guests' min='0' max='5' required />
+                    </div>
+                    <div className='rsvp-form-field h-50'>
+                      <textarea
+                        rows='4'
+                        placeholder='Any accommodations such as wheelchair access or dietary restrictions?'
+                        name='comments'
+                      />
+                    </div>
+                    <div className='rsvp-form-field h-50'>
+                      <textarea
+                        rows='4'
+                        placeholder='Your Message to the bride and groom'
+                        name='message'
+                      />
+                    </div>
+                  </FormContentGrid>
                   <button type='submit' className='rsvp-form-submit'>
                     <span>Send</span>
                   </button>
