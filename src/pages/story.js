faaf76d2f9f8d688e6d5429ecf10cedcb8fd2659
Seiji Naganuma <snaganumas@gmail.com>
2021-01-24 17:18:46 -1000

    Story copy and higher quality images (#22)
    * fix accessibility violations and hyperlink
    
    * update story copy and add an image
    
    * update images to be better quality

--- src/pages/story.js
+++ src/pages/story.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { graphql } from 'gatsby'
 
 import Layout from 'components/layout';
 import StoryHeader from 'components/story/header';
@@ -6,13 +7,33 @@ import DesktopNav from 'components/shared/desktopNav';
 import MobileNav from 'components/shared/mobileNav';
 import StoryMain from 'components/story/main';
 
-const StoryPage = () => (
+const StoryPage = ({ data }) => (
   <Layout>
     <StoryHeader />
     <DesktopNav />
     <MobileNav />
-    <StoryMain />
+    <StoryMain data={data} />
   </Layout>
 );
+
+export const query = graphql`
+  query {
+    timeline1: file(relativePath: { eq: "timeline1.jpg" }) {
+      childImageSharp {
+        gatsbyImageData(layout: FLUID, maxWidth: 580, placeholder: BLURRED)
+      }
+    }
+    timeline2: file(relativePath: { eq: "timeline2.jpg" }) {
+      childImageSharp {
+        gatsbyImageData(layout: FLUID, maxWidth: 580, placeholder: BLURRED)
+      }
+    }
+    timeline3: file(relativePath: { eq: "timeline3.jpg" }) {
+      childImageSharp {
+        gatsbyImageData(layout: FLUID, maxWidth: 580, placeholder: BLURRED)
+      }
+    }
+  }
+`;
 
 export default StoryPage;
